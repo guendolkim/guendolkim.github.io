6f7381f95e37f712d6e920f5f8831a6a84cab438
guendolkim <superjun1985@gmail.com>
2021-04-16 15:02:30 +0900

    profile ver09

--- js/mypage.js
+++ js/mypage.js
@@ -10,23 +10,28 @@ mrKim.addEventListener('click',()=>{
     profile.style.display ='none';
   }else{
     profile.style.display = 'flex';
-    slideWrapper.style.display ='none';
+    didContainer.style.display ='none';
     doingWrapper.style.display ='none';
+    noteContainer.style.display ='none';
   }
 });
 
-// did - slide
+// did - slide, tetris
+
+const didContainer = document.querySelector('.did-container');
 const $did = document.querySelector('nav> ul> li:nth-child(2)');
-const slideWrapper = document.querySelector('.slide-wrapper');
+//const slideWrapper = document.querySelector('.slide-wrapper');
+
 
 $did.addEventListener('click',()=>{
   console.log("did click");
-  if(slideWrapper.style.display == 'block'){
-    slideWrapper.style.display ='none';
+  if(didContainer.style.display == 'block'){
+    didContainer.style.display ='none';
   }else{
-    slideWrapper.style.display = 'block';
+    didContainer.style.display = 'block';
     profile.style.display ='none';
     doingWrapper.style.display ='none';
+    noteContainer.style.display ='none';
   }
 });
 
@@ -41,6 +46,24 @@ $doing.addEventListener('click',()=>{
   }else{
     doingWrapper.style.display = 'block';
     profile.style.display ='none';
-    slideWrapper.style.display ='none';
+    didContainer.style.display ='none';
+    noteContainer.style.display ='none';
+  }
+});
+
+
+//note jswing
+const $note = document.querySelector('nav> ul> li:nth-child(4)');
+const noteContainer = document.querySelector('.note-container');
+
+$note.addEventListener('click',()=>{
+  console.log("note click");
+  if(noteContainer.style.display == 'block'){
+    noteContainer.style.display ='none';
+  }else{
+    noteContainer.style.display = 'block';
+    profile.style.display ='none';
+    didContainer.style.display ='none';
+    doingWrapper.style.display ='none';
   }
 });
